f1372ab1e09c452b24e4b5eaf7153a908cabfb4c
Sashka <ivanov5oleksandr@gmail.com>
2023-02-03 20:38:20 +0200

    feat: add validation for artist DTO

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   dotenv.config();
   const port = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port, () => console.log('app is running on', port));
 }
 bootstrap();
